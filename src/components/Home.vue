e06499e0e63a0f3bc0520b88d194d2b0d13d267d
izumiyokouchi <izumi.yokouchi@opentone.co.jp>
2019-06-06 15:13:07 +0900

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <ul id="demo">
+      <tree-item
+        class="item"
+        :item="treeData"
+        @make-folder="makeFolder"
+        @add-item="addItem"
+      ></tree-item>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+import treeItem from './TreeItem.vue'
+
+var treeData = {
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+export default {
+  name: 'demo',
+  components: {treeItem},
+  data: () => {
+    return {
+      treeData: treeData
+    }
+  },
+  methods: {
+    makeFolder: function (item) {
+      this.$set(item, 'children', [])
+      this.addItem(item)
+    },
+    addItem: function (item) {
+      item.children.push({
+        name: 'new stuff'
+      })
+    }
+  }
+}
+</script>
+
